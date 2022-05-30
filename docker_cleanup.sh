2222d76c43a95411f1c61c22ae5a6cea227a94a2
Jane Hitchcock <jane.hitchcock@konghq.com>
2022-05-30 18:51:11 -0500

    Fixing up docker_cleanup script

--- docker_cleanup.sh
+++ docker_cleanup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Stopping all containers..."
+docker container stop $(docker ps -aq)
+echo "Removing all containers..."
 docker container rm $(docker ps -aq)
+echo "Removing all images..."
 docker rmi $(docker image ls -q)
+echo "Removing all volumes..."
 docker volume rm $(docker volume ls -q)
 
 #rm -rf docker-compose.yml
