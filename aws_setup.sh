258301a01ec332830db7433c64978c56d64cd9e2
Jane Hitchcock <jane.hitchcock@konghq.com>
2023-11-30 14:26:53 -0600

    More setup

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # for new AWS instance
 
-sudo yum install git -y
-sudo yum install make -y
-sudo yum install docker -y
+sudo yum install git make docker -y
+python3 -m ensurepip
+python3 -m pip install httpie
+
 sudo service docker start
+sudo chmod 666 /var/run/docker.sock
+docker ps
 
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
+git clone https://github.com/Kong/gateway-docker-compose-generator.git
